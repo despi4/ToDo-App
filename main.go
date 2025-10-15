f7f9f10af20086ea613834eb5631c6936a5bc725
despi4 <johapo62@gmail.com>
2025-10-15 13:12:44 +0500

    minor changes

--- main.go
+++ main.go
@@ -1,36 +1,4 @@
 package main
 
-import (
-	"html/template"
-	"log"
-	"net/http"
-)
-
-type message struct {
-	Msg string
-}
-
 func main() {
-	tmpl, err := template.ParseFiles("index.html")
-	if err != nil {
-		return
-	}
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, "Failed to parse form", http.StatusInternalServerError)
-			return
-		}
-
-		err = tmpl.ExecuteTemplate(w, "index.html", nil)
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-	})
-
-	http.ListenAndServe("localhost:5000", mux)
-	log.Println("Server starting : 5000")
 }
