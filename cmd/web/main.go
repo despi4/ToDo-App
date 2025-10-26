ef9f4ce65d0e4a0fb9f8fb02fe3d3422bf742d81
despi4 <johapo62@gmail.com>
2025-10-26 21:49:09 +0500

    minor changes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	handlers "todo-app/internal/handlers"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/", handlers.Home)
 
 	log.Println("Server started on:8081...")
 	err := http.ListenAndServe(":8081", mux)
