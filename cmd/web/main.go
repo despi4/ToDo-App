d258d5998aed669c50e8b296124ed2f5f5a3f4f1
despi4 <johapo62@gmail.com>
2025-11-29 00:01:50 +0500

    dd

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,47 +1,15 @@
 package main
 
 import (
-	"bufio"
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 
-	"todo-app/internal/config"
 	"todo-app/internal/handlers"
 	"todo-app/internal/repository"
 	"todo-app/internal/service"
 )
 
 func main() {
-	err := LoadDotEnv()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		log.Fatal("Error loading configuration: ", err)
-	}
-
-	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
-		cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBHost, cfg.DBPort)
-
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal("Error connecting to the database: ", err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Error pinging database: ", err)
-	}
-
-	fmt.Println("Successfully connected to the PostgreSQL database!")
-
 	dbl := repository.NewDatabase()
 	service := service.NewTodoService(dbl)
 	handler := handlers.NewTodoHandler(service)
@@ -54,42 +22,10 @@ func main() {
 	mux.HandleFunc("/todos/delete", handler.DeleteTodoHandler)
 
 	log.Print("Server started on 8081...")
-	err = http.ListenAndServe(":8081", mux)
+	err := http.ListenAndServe(":8081", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func LoadDotEnv() error {
-	file, err := os.Open("./.env")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		if strings.TrimSpace(line) == "" || strings.HasPrefix(line, "#") {
-			continue
-		}
-
-		subLine := strings.SplitN(line, "=", 2)
-		if len(subLine) != 2 {
-			continue
-		}
-
-		var (
-			key   = strings.TrimSpace(subLine[0])
-			value = strings.Trim(strings.TrimSpace(subLine[1]), "\"")
-		)
-
-		os.Setenv(key, value)
-	}
-
-	return scanner.Err()
 }
 
 // URL - endpoints | http://librarian.com/books
