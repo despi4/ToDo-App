931227c44d415decb9b9ebeba752dc12d570bac1
despi4 <johapo62@gmail.com>
2026-01-30 22:19:35 +0500

    base structure

---
+++ internal/domain/todos/todo.go
@@ -0,0 +1,29 @@
+package todos
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type TodoStatus string
+
+const (
+	TodoStatusNotStarted TodoStatus = "Not Started"
+	TodoStatusInProgress TodoStatus = "In Progress"
+	TodoStatusDone       TodoStatus = "Done"
+)
+
+type Todo struct {
+	Id        uuid.UUID
+	Name      string
+	Task      string
+	Status    TodoStatus
+	CreatedAt time.Time
+}
+
+var (
+	ErrNotFound        = errors.New("not found")
+	ErrInvalidArgument = errors.New("invalid argumenmt")
+)
