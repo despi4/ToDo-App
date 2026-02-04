6bd92d0a51d9fce5267cb3618d32df568404435b
despi4 <johapo62@gmail.com>
2026-02-04 18:46:22 +0500

    feat: handlers

--- internal/app/app.go
+++ internal/app/app.go
@@ -21,12 +21,18 @@ func Init() {
 		log.Fatal(err)
 	}
 
+	// 2458b756-daef-4978-861d-557e0671979d
+	// ivan@example.com
 	repo := postgre.NewUserRepo(db)
 	service := usersvc.NewUserService(repo)
 	handler := userhandler.NewUserHandler(service)
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /users", handler.Create)
+	router.HandleFunc("GET /users/{id}", handler.GetByID)
+	router.HandleFunc("GET /users/", handler.GetByEmail)
+	router.HandleFunc("PATCH /users/{id}", handler.Update)
+	router.HandleFunc("DELETE /users/{id}", handler.Delete)
 
 	log.Println("Server started on : 3000")
 	err = http.ListenAndServe(":3000", router)
--- internal/transport/http/handler/user/handler.go
+++ internal/transport/http/handler/user/handler.go
@@ -9,6 +9,8 @@ import (
 	"time"
 	userdomain "todo-app/internal/domain/user"
 	userdto "todo-app/internal/transport/http/dto/user"
+
+	"github.com/google/uuid"
 )
 
 type UserHandler struct {
@@ -29,28 +31,13 @@ func (userHandler *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "invalid json", http.StatusBadRequest)
 		return
 	}
+	defer r.Body.Close()
 
 	var (
 		name    = strings.TrimSpace(creatReq.Name)
 		surname = strings.TrimSpace(creatReq.Surname)
 		email   = strings.TrimSpace(creatReq.Email)
 	)
-
-	// 2) Validation
-	if name == "" {
-		http.Error(w, "name is required", http.StatusBadRequest)
-		return
-	}
-
-	if surname == "" {
-		http.Error(w, "surname is required", http.StatusBadRequest)
-		return
-	}
-
-	if email == "" {
-		http.Error(w, "email is required", http.StatusBadRequest)
-		return
-	}
 
 	ctx := r.Context()
 	ctx, cancel := context.WithTimeout(ctx, 1*time.Minute)
@@ -64,14 +51,88 @@ func (userHandler *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 
 	out, err := userHandler.service.Create(ctx, user)
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	log.Println("New User created")
+	log.Printf("ID: %s, Name: %s, Surname: %s, Email: %s, CreatedAt: %s, UpdteadAt: %s\n", out.ID, out.Name, out.Surname, out.Email, out.CreatedAt, out.UpdatedAt)
+}
+
+func (userHandler *UserHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Path[7:]
+
+	id, err := uuid.Parse(url)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if id == uuid.Nil {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+
+	user, err := userHandler.service.GetByID(ctx, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	log.Println("Get User By ID")
+	log.Printf("ID: %s, Name: %s, Surname: %s, Email: %s, CreatedAt: %s, UpdteadAt: %s\n", user.ID, user.Name, user.Surname, user.Email, user.CreatedAt, user.UpdatedAt)
+}
+
+func (userHandler *UserHandler) GetByEmail(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	defer r.Body.Close()
+
+	queryEmail := queryParams.Get("email")
+
+	email := strings.TrimSpace(queryEmail)
+	if email == "" {
+		http.Error(w, "email is requied", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+
+	user, err := userHandler.service.GetByEmail(ctx, email)
+	if err != nil {
 		http.Error(w, "service error", http.StatusInternalServerError)
 		return
 	}
 
-	log.Println(out)
+	w.WriteHeader(http.StatusOK)
+
+	log.Println("Get User By Email")
+	log.Printf("ID: %s, Name: %s, Surname: %s, Email: %s, CreatedAt: %s, UpdteadAt: %s\n", user.ID, user.Name, user.Surname, user.Email, user.CreatedAt, user.UpdatedAt)
 }
 
 func (userHandler *UserHandler) Update(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[:7]
+
+	id, err := uuid.Parse(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if id == uuid.Nil {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+
 	var updateReq userdto.UpdateUserRequest
 
 	if err := json.NewDecoder(r.Body).Decode(&updateReq); err != nil {
@@ -83,9 +144,56 @@ func (userHandler *UserHandler) Update(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(ctx, 1*time.Minute)
 	defer cancel()
 
-	// out, err := userHandler.service.Update(ctx, ID)
+	user, err := userHandler.service.Update(ctx, id, userdomain.UserUpdate(updateReq))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	log.Println("User updated")
+	log.Println("Updated rows")
+
+	if updateReq.Name != nil {
+		log.Println("name: ", user.Name)
+	}
+
+	if updateReq.Surname != nil {
+		log.Println("surname: ", user.Surname)
+	}
+
+	if updateReq.Email != nil {
+		log.Println("email: ", user.Email)
+	}
 }
 
 func (userHandler *UserHandler) Delete(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	defer r.Body.Close()
 
+	queryID := queryParams.Get("id")
+
+	id, err := uuid.Parse(queryID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if id != uuid.Nil {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+
+	err = userHandler.service.Delete(ctx, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
