a5b32613365d56a4affd80996f5ffc66e01b4805
despi4 <johapo62@gmail.com>
2026-03-04 22:50:20 +0500

    feat: migratino and compose.yml

---
+++ migrations/001_init.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    id bigserial primary key,
+    user_role varchar(50) not null,
+    first_name varchar(50) not null,
+    last_name varchar(50) not null,
+    email text unique not null,
+    created_at timestamptz default now(),
+    updated_at timestamptz default now(),
+);
+
+create or replace function update_modified_column()
+returns trigger as $$
+begin
+    new.updated_at = now();
+    return new;
+end;
+$$ language 'plpgsql';
+
+create trigger update_user_modtime
+    before update on users
+    for each row
+    execute procedure update_modified_column();
